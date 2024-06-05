169b0681fe5a12a4901990a335751ef913a54499
jaypanchal261 <96589430+jaypanchal261@users.noreply.github.com>
2024-06-05 13:21:41 +0530

    /tmp changed

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/temp')
+      cb(null, '/tmp')
     },
     filename: function (req, file, cb) {
       
